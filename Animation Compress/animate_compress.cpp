2b6bb3666e01d7f357bfcdd34f5f59abcc6e4390
jeremybeeman <jeremybeemandesigner@gmail.com>
2022-05-11 09:04:49 -0500

    Set up allocating and deallocating memory for BMP reading and writing. Verified via valgrind

--- Animation Compress/animate_compress.cpp
+++ Animation Compress/animate_compress.cpp
@@ -22,12 +22,14 @@
 #define max_SRAM_bytes 1024*8
 #define max_Flash_bytes 1024*256
 
-#define start_expected_animation_files 1 //CHANGE TO A MORE REASONABLE NUMBER AFTER TESTING
-#define grow_files_multiplier 2
-
+#define buffer_animation_files 3 //0th for first file, 1st last file buffer, 2nd for current file
+#define buffer_first_file 0
+#define buffer_last_file 1
+#define buffer_curr_file 2
 
 //gcc -Wall -Werror animate_compress.cpp -o animate_compress.exe
 //animate_compress.exe "D:\jjbee\OneDrive\projects\Art\Cotton Candy\Pink_Cotton_Candy\Blinking\BMP" this 
+//valgrind --leak-check=yes --track-origins=yes  ./animate_compress "Pictures" this
 
 
 bool verify_bmp(FILE* curr_file, int* bmp_size, int* bmp_offset)
@@ -91,27 +93,24 @@ bool verify_bmp(FILE* curr_file, int* bmp_size, int* bmp_offset)
 int main(int argc, char *argv[])
 {
     char input_dir_str[100];
-    int max_file_count = start_expected_animation_files;
-    int16_t** BMP_animation_files = (int16_t **)malloc(sizeof(int16_t *)*start_expected_animation_files); 
+    long unsigned int last_byte_size = 0;
     //long running_byte_count = 0;//add eventually. Will keep track of bytes
 
     if (argc < 3)
         printf("Usage: (animate_compress.exe in_directory out_directory)\n");
     else {
+        int16_t** BMP_pixel_array_buffer = (int16_t **)malloc(sizeof(int16_t *)*buffer_animation_files); 
         strcpy(input_dir_str, argv[1]);
         DIR * FD; 
         if (NULL == (FD = opendir(argv[1]))) {
             fprintf(stderr, "ERROR: Couldn't open directory\n");
+            free(BMP_pixel_array_buffer);
             return 1;
         }
     char input_file_str[200];
     struct dirent* fd_file_name;
     int file_count = 0;
     while((fd_file_name = readdir(FD))) {
-        if (file_count >= max_file_count) {
-            BMP_animation_files = (int16_t **)realloc(BMP_animation_files, sizeof(int16_t *)*max_file_count*grow_files_multiplier);
-            max_file_count = max_file_count*grow_files_multiplier;
-        }
         //printf("File: %s\n", fd_file_name->d_name);
         if (!strcmp(fd_file_name->d_name, "."))
             continue; 
@@ -119,12 +118,17 @@ int main(int argc, char *argv[])
             continue; 
 
         strcpy(input_file_str, input_dir_str);
-        strcat(input_file_str, "\\");
+        strcat(input_file_str, "/"); //need to change for windows. On WSL
         strcat(input_file_str, fd_file_name->d_name);
-        //printf("Data: %s\n", input_file_str);
+        printf("Data: %s\n", input_file_str);
         FILE * curr_file = fopen(input_file_str, "rb"); 
         if (curr_file == NULL) {
             fprintf(stderr, "ERROR, Failed to open file [%s]\n", fd_file_name->d_name);
+            for (int i = 0; i < 2*(file_count > 0)+(file_count>1); i++) {
+                free(BMP_pixel_array_buffer[i]);
+            }
+            free(BMP_pixel_array_buffer);
+            closedir(FD);
             return 1;
         }
 
@@ -133,16 +137,66 @@ int main(int argc, char *argv[])
         
         if(!verify_bmp(curr_file, &bmp_size, &bmp_offset)) {
             fprintf(stderr, "Exiting Due to Failed BMP...\n");
+            for (int i = 0; i < 2*(file_count > 0)+(file_count>1); i++) {
+                free(BMP_pixel_array_buffer[i]);
+            }
+            free(BMP_pixel_array_buffer);
+            closedir(FD);
             return 1;
         }
         
         fprintf(stdout, "File Name: %s; bmp size: %x; bmp offset: %x; \n", fd_file_name->d_name, bmp_size, bmp_offset);
         
+        //when on the first file, it gets stored into the 0th part of the buffer        
+        if (file_count == 0) {
+            BMP_pixel_array_buffer[buffer_first_file] = (int16_t*)malloc(sizeof(int16_t)*(bmp_size-bmp_offset)); 
+            BMP_pixel_array_buffer[buffer_last_file] = (int16_t*)malloc(sizeof(int16_t)*(bmp_size-bmp_offset)); 
+            last_byte_size = sizeof(int16_t)*(bmp_size-bmp_offset);
+        }
+        else if (file_count == 1){
+            if (last_byte_size != sizeof(int16_t)*(bmp_size-bmp_offset)) {
+                fprintf(stderr, "Byte Size of Images are Different. File Numbers: [%d->%d] Exiting...\n", 0, 1);
+                free(BMP_pixel_array_buffer[buffer_first_file]);
+                free(BMP_pixel_array_buffer[buffer_last_file]);
+                free(BMP_pixel_array_buffer);
+                closedir(FD);
+                return 1;
+            }
+            //Now that the size is valid, allocate the space and analyze it next to the first image
+            BMP_pixel_array_buffer[buffer_curr_file] = (int16_t*)malloc(sizeof(int16_t)*(bmp_size-bmp_offset)); 
+            last_byte_size = sizeof(int16_t)*(bmp_size-bmp_offset);
+        }
+        else {
+            if (last_byte_size != sizeof(int16_t)*(bmp_size-bmp_offset)) {
+                fprintf(stderr, "Byte Size of Images are Different. File Numbers: [%d->%d] Exiting...\n", file_count-1, file_count);
+                for (int i = 0; i < buffer_animation_files; i++) {
+                    free(BMP_pixel_array_buffer[i]);
+                }
+                free(BMP_pixel_array_buffer);
+                closedir(FD);
+                return 1;
+            }
+
+            last_byte_size = sizeof(int16_t)*(bmp_size-bmp_offset);
+        }
         
         fclose(curr_file);
         file_count++;
     }
     closedir(FD);
+    if (file_count == 1) {
+        fprintf(stderr, "There was only one file in the folder, so no animation was possible.\n");
+        for (int i = 0; i < buffer_animation_files-1; i++) {
+            free(BMP_pixel_array_buffer[i]);
+        }
+        free(BMP_pixel_array_buffer);
+        return 1;
+    }
+
+    for (int i = 0; i < buffer_animation_files; i++) {
+        free(BMP_pixel_array_buffer[i]);
+    }
+    free(BMP_pixel_array_buffer);
   }
   return 0;
 }
